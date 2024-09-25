a00ec938f4ae6adac0667345642e217e72f8dec9
AlfayezDev <mohammed@alfayez.dev>
2024-09-25 21:31:35 +0300

    wip

--- products/restaurant/merchant-website/vite.config.ts
+++ products/restaurant/merchant-website/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
+//@ts-ignore
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+  plugins: [TanStackRouterVite(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
